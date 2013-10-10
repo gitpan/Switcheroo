4775d37e41499a3bc0b83d3c3b7fe0f286259609
Toby Inkster <tobyink@cpan.org>
2013-10-10 13:10:38 +0000

    Import of TOBYINK/Switcheroo-0.006 from CPAN.
    gitpan-cpan-distribution: Switcheroo
    gitpan-cpan-version:      0.006
    gitpan-cpan-path:         TOBYINK/Switcheroo-0.006.tar.gz
    gitpan-cpan-author:       TOBYINK
    gitpan-cpan-maturity:     released

--- lib/Switcheroo.pm
+++ lib/Switcheroo.pm
@@ -5,13 +5,14 @@ use warnings;
 package Switcheroo;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.005';
+our $VERSION   = '0.006';
 our @EXPORT    = qw( switch );
 our @EXPORT_OK = qw( match );
+our @ISA       = qw( Exporter::Tiny );
 
 use Devel::Caller qw( caller_args );
 use Devel::LexAlias qw( lexalias );
-use Exporter qw( );
+use Exporter::Tiny qw( );
 use match::simple qw( match );
 use PadWalker qw( peek_my );
 use Parse::Keyword { switch => \&_parse_switch };
@@ -20,7 +21,7 @@ sub import
 {
 	my $pkg = caller;
 	eval qq[ package $pkg; our \$a; our \$b; ];
-	goto \&Exporter::import;
+	goto \&Exporter::Tiny::import;
 }
 
 sub switch
@@ -52,19 +53,19 @@ sub switch
 	CASE: for my $case ( @$cases )
 	{
 		my ($type, $condition, $block) = @$case;
-		lexalias($condition, $_, $pad->{$_}) for keys %$pad;
 		
 		my $matched = 0;
 		if ($type eq 'block')
 		{
+			lexalias($condition, $_, $pad->{$_}) for keys %$pad;
 			$matched = !!$condition->(@args);
 		}
 		else
 		{
-			my @terms = $condition->();
-			TERM: for my $term (@terms)
+			TERM: for my $term (@$condition)
 			{
-				$match->($var, $term) ? (++$matched && last TERM) : next TERM;
+				lexalias($term, $_, $pad->{$_}) for keys %$pad;
+				$match->($var, $term->()) ? (++$matched && last TERM) : next TERM;
 			}
 		}
 		
@@ -154,6 +155,19 @@ sub _parse_switch
 	);
 }
 
+sub _munge_term
+{
+	if (lex_peek(1) eq '/')
+	{
+		lex_stuff('qr');
+	}
+	elsif (lex_peek(2) =~ /m\W/)
+	{
+		lex_read(1);
+		lex_stuff('qr');
+	}
+}
+
 sub _parse_case
 {
 	my ($expr, $type);
@@ -163,7 +177,7 @@ sub _parse_case
 	{
 		lex_read(1);
 		$type = 'term';
-		$expr = parse_fullexpr;
+		$expr = _parse_list_of_terms(\&_munge_term);
 		lex_read_space;
 		die "syntax error; expected close parenthesis" unless lex_peek eq ')';
 		lex_read(1);
@@ -179,18 +193,8 @@ sub _parse_case
 	
 	else
 	{
-#		if (lex_peek(1) eq '/')
-#		{
-#			lex_stuff('qr');
-#		}
-#		elsif (lex_peek(2) =~ /m\W/)
-#		{
-#			lex_read(1);
-#			lex_stuff('qr');
-#		}
-#		
 		$type = 'simple-term';
-		$expr = parse_fullexpr;
+		$expr = _parse_list_of_terms(\&_munge_term);
 		lex_read_space;
 	}
 	
@@ -200,6 +204,28 @@ sub _parse_case
 	
 	my $block = _parse_consequence();
 	return [ $type, $expr, $block ];
+}
+
+sub _parse_list_of_terms
+{
+	my $munge = shift;
+	
+	my @expr;
+	lex_read_space;
+	$munge->() if $munge;
+	push @expr, parse_termexpr;
+	lex_read_space;
+	
+	while (lex_peek eq ',')
+	{
+		lex_read(1);
+		lex_read_space;
+		$munge->() if $munge;
+		push @expr, parse_termexpr;
+		lex_read_space;
+	}
+	
+	return \@expr;
 }
 
 sub _parse_consequence
@@ -303,6 +329,14 @@ a boolean.
       case { $_ % 2 }:  say "an odd number";
       default:          say "an even number";
    }
+
+=head2 Regexp Expressions
+
+If this module encounters:
+
+   switch ($foo) { case /foo/: say "foo" }
+
+Don't worry; we know you meant C<< qr/foo/ >> and not C<< m/foo/ >>.
 
 =head2 Statement blocks
 
